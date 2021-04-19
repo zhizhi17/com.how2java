c4f80dc409fd1c9474d88bf4058b3aa51263e95c
692B7452DBC5911250B7D10600CBC777 <692B7452DBC5911250B7D10600CBC777@i-search.com.cn>
2021-04-20 02:49:37 +0800

    Initial commit

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.util.Date;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("Hi好中国");
+        response.getWriter().println(new Date().getTime());
+        System.out.println("nihoasfasdfsafsafsafas11111111");
+    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+}
